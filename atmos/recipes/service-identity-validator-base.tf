a009441b811d776f889861b5e77591cb6e23acb4
Dave Pease <dpease@drift.com>
2023-11-27 12:27:41 -0800

    removed atmos

--- atmos/recipes/service-identity-validator-base.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "upstream" {
-  backend = "s3"
-  config = {
-    bucket = var.terraform_bucket_name
-    region = "us-east-1"
-    key    = "${var.atmos_env}/terraform.tfstate"
-  }
-}
